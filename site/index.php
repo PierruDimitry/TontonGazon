be914d5dfce382e2ea5d7e97c0c82d88dc0a183e
Unknown <dondaine.mathieu@gmail.com>
2019-01-18 17:46:26 +0100

    Just starting

---
+++ site/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Accueil - Tonton Gazon</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Description de Tonton Gazon">
+    <meta name="keywords" content="référencement">
+    <meta name="author" content="Dondaine Mathieu & Pierru Dimitry">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+    <body>
+        
+        <div id="test">
+            <p>Ceci est un test</p>
+        </div>
+
+    </body>
+
+</html>
+
--- site/vues/head.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <title>Accueil - Tonton Gazon</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="Description de Tonton Gazon">
-    <meta name="keywords" content="référencement">
-    <meta name="author" content="Dondaine Mathieu & Pierru Dimitry">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
