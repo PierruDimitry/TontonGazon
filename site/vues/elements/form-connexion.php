894f28739733500e6139b9dbd92bd278ae7e2da5
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-01-23 10:44:43 +0100

    Ajout formulaire de connexion

---
+++ site/vues/elements/form-connexion.php
@@ -0,0 +1,14 @@
+<div class="from" id="form-con">
+    <h1>Connexion</h1>
+    <form action="/action_page.php" method="POST">
+        <label>Identifiant
+            <input type="text" placeholder="Votre Identifiant ou Adresse Mail" name="identifiant" />
+        </label>
+        <label>Mot de passe
+            <input type="password" placeholder="Votre mot de passe" name="password" />
+        </label>
+        <input type="submit" value="C'est parti !" />
+    </form>
+    <a href="">J'ai oublié mon mot de passe</a>
+    <a href="">Je n'ai pas encore de compte</a>
+</div>
