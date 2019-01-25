aadef701a64b4b49a20e05b562507336ea25dc1f
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-01-25 10:09:30 +0100

    ajout ID et CLASS dans le formulaire

--- site/vues/elements/form-inscription.php
+++ site/vues/elements/form-inscription.php
@@ -1,18 +1,29 @@
 <div class="from" id="form-con">
     <h1>Inscription</h1>
-    <form action="/action_page.php" method="POST">
-        <label>Nom  <input type="text" placeholder="Votre nom" name="nom" /> </label>
-        <label>Prénom  <input type="text" placeholder="Votre prénom" name="prenom" /> </label>
-        <label>identifiant  <input type="text" placeholder="L'identifiant qui vous permettra de vous connecter" name="identifiant" /> </label>
-        <label>Adresse Mail  <input type="email" placeholder="Votre adresse Mail" name="mail" /> </label>
-        <label>Mot de passe  <input type="password" placeholder="Votre mot de passe" name="password1" /> </label>
-        <label>Vérification du Mot de passe  <input type="password" placeholder="Vérification de votre mot de passe" name="password2" /> </label>
-        <label>Adresse Ligne 1  <input type="text" placeholder="numéro de rue, rue, avenue etc" name="address1" /> </label>
-        <label>Adresse Ligne 2  <input type="text" placeholder="apartement , porte, etage etc" name="address2" /> </label>
-        <label>Ville  <input type="text" placeholder="exemple : Douai" name="city" /> </label>
-        <label>code Postal  <input type="text" placeholder="exemple : 62300" name="postal_code" /> </label>
+    <form class="form" id="form-insc" action="/action_page.php" method="POST">
+        <label> Nom
+            <input type="text" placeholder="Votre nom" id="form-i-nom" name="nom" />
+        </label> <label> Prénom
+            <input type="text" placeholder="Votre prénom" id="form-i-prenom" name="prenom" />
+        </label> <label> identifiant
+            <input type="text" placeholder="L'identifiant qui vous permettra de vous connecter" id="form-i-identifiant" name="identifiant" />
+        </label> <label> Adresse Mail
+            <input type="email" placeholder="Votre adresse Mail" id="form-i-mail" name="mail" />
+        </label> <label> Mot de passe
+            <input type="password" placeholder="Votre mot de passe" id="form-i-password1" name="password1" />
+        </label> <label> Vérification du Mot de passe
+            <input type="password" placeholder="Vérification de votre mot de passe" id="form-i-password2" name="password2" />
+        </label> <label> Adresse Ligne 1
+            <input type="text" placeholder="numéro de rue, rue, avenue etc" id="form-i-address1" name="address1" />
+        </label> <label> Adresse Ligne 2
+            <input type="text" placeholder="apartement , porte, etage etc" id="form-i-address2" name="address2" />
+        </label> <label class="2in1"> Ville
+            <input type="text" placeholder="exemple : Douai" id="form-i-city" name="city" />
+        </label> <label class="2in1"> Code Postal
+            <input type="text" placeholder="exemple : 62300" id="form-i-postal" name="postal_code" />
+        </label>
 
-        <input type="submit" value="C'est parti !" />
+        <input class="form-submit" type="submit" value="C'est parti !" />
     </form>
     <a href="">J'ai déja un compte</a>
     <a href="">J'ai besoin d'aide</a>
