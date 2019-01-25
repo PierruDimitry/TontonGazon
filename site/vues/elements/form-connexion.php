1266d7875e21935364ee7021b50c7e841bca72cb
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-01-25 10:09:19 +0100

    ajout ID et CLASS dans le formulaire

--- site/vues/elements/form-connexion.php
+++ site/vues/elements/form-connexion.php
@@ -1,12 +1,12 @@
 <div class="from" id="form-con">
     <h1>Connexion</h1>
-    <form action="/action_page.php" method="POST">
-        <label>Identifiant
-            <input type="text" placeholder="Votre Identifiant ou Adresse Mail" name="identifiant" />
+    <form class="form" id="form-insc" action="/action_page.php" method="POST">
+        <label> Identifiant 
+            <input type="text" placeholder="Votre Identifiant ou Adresse Mail" id="form-c-identifiant" name="identifiant" /> 
+        </label> <label> Mot de passe 
+            <input type="password" placeholder="Votre mot de passe" id="form-c-password" name="password" /> 
         </label>
-        <label>Mot de passe
-            <input type="password" placeholder="Votre mot de passe" name="password" />
-        </label>
+
         <input type="submit" value="C'est parti !" />
     </form>
     <a href="">J'ai oublié mon mot de passe</a>
