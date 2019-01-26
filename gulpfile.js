25d82f442aaf424dd2af81e19b144fadca19993f
DondaineMathieu <dondaine.mathieu@gmail.com>
2019-01-26 16:01:59 +0100

    ajout dun package gulp pour lauto prefixe

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,18 @@ gulp.task('sass', function(){
       .pipe(gulp.dest('./site/css'));
   });
 
+  gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+  
+    return gulp.src('./site/css/style.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./site/css'));
+  });
+
   gulp.task('watch', function(){
-    gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./sass/**/*.scss', gulp.series('sass','autoprefixer'));
   })
